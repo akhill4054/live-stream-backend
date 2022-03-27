e0393743c81549e4a6e2687bf1161a5096fc46ad
Akhill4054 <distanceuploaders@gmail.com>
2022-03-28 02:33:09 +0530

    Revet Flask app name and initialization changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn faskr:app
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -1,36 +1,36 @@
-# import base64
-# import json
-# import os
+import base64
+import json
+import os
 
-# import firebase_admin
-# from firebase_admin import credentials
-# from flask import Flask
-
-
-# # Init firebase-admin.
-# firebase_app_credential = credentials.Certificate(json.loads(base64.b64decode(os.environ["GOOGLE_APPLICATION_CREDS_BASE64"])))
-# default_app = firebase_admin.initialize_app(credential=firebase_app_credential)
+import firebase_admin
+from firebase_admin import credentials
+from flask import Flask
 
 
-# # create and configure the app
-# app = Flask(__name__, instance_relative_config=True)
+# Init firebase-admin.
+firebase_app_credential = credentials.Certificate(json.loads(base64.b64decode(os.environ["GOOGLE_APPLICATION_CREDS_BASE64"])))
+default_app = firebase_admin.initialize_app(credential=firebase_app_credential)
 
-# # Setup configs.
-# app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 
-# # ensure the instance folder exists
-# try:
-#     os.makedirs(app.instance_path)
-# except OSError:
-#     pass
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
 
-# # Import and register blueprints
-# from index.v1.views import index_bp
-# from auth.v1.views import auth_bp
-# from users.v1.views import users_bp
-# from streamings.v1.views import streamins_bp
+# Setup configs.
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 
-# app.register_blueprint(index_bp)
-# app.register_blueprint(auth_bp)
-# app.register_blueprint(users_bp)
-# app.register_blueprint(streamins_bp)
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+# Import and register blueprints
+from index.v1.views import index_bp
+from auth.v1.views import auth_bp
+from users.v1.views import users_bp
+from streamings.v1.views import streamins_bp
+
+app.register_blueprint(index_bp)
+app.register_blueprint(auth_bp)
+app.register_blueprint(users_bp)
+app.register_blueprint(streamins_bp)
