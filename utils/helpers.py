96422239f8869afce7060de71ce9a2f0e6728326
Akhill4054 <distanceuploaders@gmail.com>
2022-04-04 21:52:19 +0530

    Fix image with '.' in it's name not uploading properly

--- utils/helpers.py
+++ utils/helpers.py
@@ -6,10 +6,11 @@ from utils.response_helpers import get_invalid_request_response
 
 
 def upload_image(image_file: FileStorage, path_without_extension: str) -> tuple:
-    image_file_extension = image_file.filename.split(".")[1]
+    split_filename = image_file.filename.split(".")
+    image_file_extension = split_filename[len(split_filename) - 1]
 
     if not image_file_extension.upper() in ALLOWED_IMAGE_EXTENSIONS:
-        return get_invalid_request_response(message=f"Image type {image_file_extension.upper()} is not allowed.")
+        raise Exception(f"Image type {image_file_extension.upper()} is not allowed.")
 
     full_file_path = f"{path_without_extension}.{image_file_extension}"
 
